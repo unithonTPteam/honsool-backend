e08f8902d21618d86cd3b23f14492b62380870af
hackurity <hackurity01@gmail.com>
2017-07-30 00:04:33 +0900

    socket

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+
 var app = express();
+
+//var IO = require('socket.io').listen(server);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+//Socket(server);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,6 +48,14 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+const corsOptions = {
+    methods       : "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    exposedHeaders: "x-auth",
+};
+
+app.use(cors(corsOptions));
+app.use(cors());
 
 // branch test 
 
---
+++ routes/chating.js
@@ -0,0 +1,17 @@
+var IO = require('socket.io');
+//import IO                   from 'socket.io';
+
+module.exports = (server) => {
+    const roomNum = 0;
+    const io = IO(server, {path: '/room'});
+
+    io.on('connection', function(socket) {
+        console.log('connected');
+
+        socket.on('join', (params) => {
+            console.log('message : ' + params);
+            //console.log(socket);
+            io.emit('join', 'sosososososoos');
+        });
+    });
+}
